ef2f4e3af174a3ab7960b22c563c870aa8fcfca0
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-06-19 07:08:05 -0400

    Fixed linters

--- src/modules/scoreBox.js
+++ src/modules/scoreBox.js
@@ -1,8 +1,8 @@
 const scoreBox = () => {
-    const scoreBoard = document.getElementById('score-board');
-    const scoreBoxWrapper = document.createElement('section');
-    scoreBoxWrapper.classList.add('score-box');
-    scoreBoxWrapper.innerHTML = `
+  const scoreBoard = document.getElementById('score-board');
+  const scoreBoxWrapper = document.createElement('section');
+  scoreBoxWrapper.classList.add('score-box');
+  scoreBoxWrapper.innerHTML = `
       <div class="block" id="1"><p class="name">Wasiu:</p><p class="score">100</p></div>
       <div class="block" id="2"><p class="name">Wasiu:</p><p class="score">100</p></div>
       <div class="block" id="3"><p class="name">Wasiu:</p><p class="score">100</p></div>
@@ -12,8 +12,6 @@ const scoreBox = () => {
       <div class="block" id="7"><p class="name">Wasiu:</p><p class="score">100</p></div>
       <div class="block" id="8"><p class="name">Wasiu:</p><p class="score">100</p></div>
       <div class="block" id="9"><p class="name">Wasiu:</p><p class="score">100</p></div>`;
-  
-    scoreBoard.appendChild(scoreBoxWrapper);
-  };
-  
-  module.exports = scoreBox;
+  scoreBoard.appendChild(scoreBoxWrapper);
+};
+module.exports = scoreBox;
