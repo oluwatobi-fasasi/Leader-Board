66740aca3fecb47b16a02208b6ffcb2b82896de9
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-06-19 06:49:32 -0400

    Add modules

---
+++ src/modules/scoreBox.js
@@ -0,0 +1,19 @@
+const scoreBox = () => {
+    const scoreBoard = document.getElementById('score-board');
+    const scoreBoxWrapper = document.createElement('section');
+    scoreBoxWrapper.classList.add('score-box');
+    scoreBoxWrapper.innerHTML = `
+      <div class="block" id="1"><p class="name">Wasiu:</p><p class="score">100</p></div>
+      <div class="block" id="2"><p class="name">Wasiu:</p><p class="score">100</p></div>
+      <div class="block" id="3"><p class="name">Wasiu:</p><p class="score">100</p></div>
+      <div class="block" id="4"><p class="name">Wasiu:</p><p class="score">100</p></div>
+      <div class="block" id="5"><p class="name">Wasiu:</p><p class="score">100</p></div>
+      <div class="block" id="6"><p class="name">Wasiu:</p><p class="score">100</p></div>
+      <div class="block" id="7"><p class="name">Wasiu:</p><p class="score">100</p></div>
+      <div class="block" id="8"><p class="name">Wasiu:</p><p class="score">100</p></div>
+      <div class="block" id="9"><p class="name">Wasiu:</p><p class="score">100</p></div>`;
+  
+    scoreBoard.appendChild(scoreBoxWrapper);
+  };
+  
+  module.exports = scoreBox;
