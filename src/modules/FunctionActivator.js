667e72a585e0a9d1cf75e192994460335094247c
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-06-20 15:36:32 -0400

    Add Script

---
+++ src/modules/FunctionActivator.js
@@ -0,0 +1,23 @@
+import createGame from "./PostFunction";
+import scoreBox from './scoreBox.js';
+
+const form = document.querySelector('form');
+const userName = document.getElementById('name-input');
+const userScore = document.getElementById('score-input');
+const refresh = document.querySelector('.refresh');
+
+function postActivator(){
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        createGame(userName.value, userScore.value);
+        form.reset();
+    })
+}
+
+function getActivator(){
+    refresh.addEventListener('click', async()=>{
+       await scoreBox();
+    })
+}
+
+export {postActivator, getActivator}
